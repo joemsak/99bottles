5446906d9fe45046fc67eb20b7ec97299cedbf12
Joe Sak <joe@joesak.com>
2020-07-15 23:19:09 -0500

    Move BottleNumber to its own file

---
+++ js/src/bottle-number.js
@@ -0,0 +1,81 @@
+export default class BottleNumber {
+  static for(number) {
+    switch(number) {
+      case 0:
+        return new BottleNumber0(number)
+      case 1:
+        return new BottleNumber1(number)
+      case 6:
+        return new BottleNumber6(number)
+      default:
+        return new BottleNumber(number)
+    }
+  }
+
+  constructor(number) {
+    this.number = number
+  }
+
+  successor() {
+    return this.number - 1
+  }
+
+  container() {
+    return 'bottles'
+  }
+
+  pronoun() {
+    return 'one'
+  }
+
+  quantity() {
+    return this.number
+  }
+
+  action() {
+    return `Take ${this.pronoun()} down, pass it around`
+  }
+
+  toString() {
+    return [this.quantity(), this.container()].join(' ')
+  }
+}
+
+class BottleNumber0 extends BottleNumber {
+  successor() {
+    return 99
+  }
+
+  action() {
+    return "Go to the store, steal some more"
+  }
+
+  quantity() {
+    return 'No more'
+  }
+}
+
+class BottleNumber1 extends BottleNumber {
+  container() {
+    return 'bottle'
+  }
+
+  pronoun() {
+    return 'it'
+  }
+}
+
+class BottleNumber6 extends BottleNumber {
+  container() {
+    return 'six-pack'
+  }
+
+  quantity() {
+    return 1
+  }
+
+  pronoun() {
+    return 'a bottle'
+  }
+}
+
